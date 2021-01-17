4c9eec3bfac3b37d2d9501b6f5ff5a4dd4e7e84d
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-17 20:15:19 +0200

    Initial commit

---
+++ src/main/java/com/company/backend/CaEndProjectApplication.java
@@ -0,0 +1,13 @@
+package com.company.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaEndProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaEndProjectApplication.class, args);
+    }
+
+}
