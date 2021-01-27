7db3daa5b17062970edb7b43f472b87927754bfe
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-27 16:28:32 +0200

    fixed Repository imports

--- src/main/java/com/company/backend/repository/UserRepository.java
+++ src/main/java/com/company/backend/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.company.backend.repository;
 
-import com.company.backend.entity.UserEntity;
+import com.company.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
