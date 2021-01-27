f06fadb7f249a5da7caf523a4ac8d108cdf5faa5
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-27 16:22:47 +0200

    removed Customer entity and repository

--- src/main/java/com/company/backend/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.backend.repository;
-
-import com.company.backend.entity.CustomerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
-}
