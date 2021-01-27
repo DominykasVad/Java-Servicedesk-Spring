3d0efa595660cc6f42ac19e11a0427bc15f7e8cd
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-27 16:26:55 +0200

    added ServiceRequest

---
+++ src/main/java/com/company/backend/entity/ServiceRequest.java
@@ -0,0 +1,48 @@
+package com.company.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = service_requests)
+public class ServiceRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "summary", nullable = false)
+    private String summary;
+
+    @Lob
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @ManyToOne
+    @JoinColumn(name = "reported_by")
+    private User reportedBy;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime updatedAt;
+
+}
