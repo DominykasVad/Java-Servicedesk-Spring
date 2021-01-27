643d01258651f920829ec1d436ee6ce654de023a
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-27 15:12:31 +0200

    grammatical fix

--- src/main/java/com/company/backend/entity/CustomerEntity.java
+++ src/main/java/com/company/backend/entity/CustomerEntity.java
@@ -24,7 +24,7 @@ public class CustomerEntity {
     @Size(min = 3, max = 254)
     private String email;
 
-    @Column(name = "phome", nullable = false, length = 50)
+    @Column(name = "phone", nullable = false, length = 50)
     private String phone;
 
     @Column(name = "address",nullable = false)
