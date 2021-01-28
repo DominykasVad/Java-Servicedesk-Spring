ba769ca2f1fb0893c2c4a86bdc3ee714d6ed1d58
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-28 15:46:00 +0200

    added ServiceRequestMapper

---
+++ src/main/java/com/company/backend/mapper/ServiceRequestMapper.java
@@ -0,0 +1,22 @@
+package com.company.backend.mapper;
+
+import com.company.backend.dto.ServiceRequestDTO;
+import com.company.backend.entity.ServiceRequest;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceRequestMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public ServiceRequestDTO convertServiceRequestToDTO(ServiceRequest serviceRequest) {
+        return modelMapper.map(serviceRequest, ServiceRequestDTO.class);
+    }
+
+    public ServiceRequest convertServiceRequestToEntity(ServiceRequestDTO serviceRequestDTO) {
+        return modelMapper.map(serviceRequestDTO, ServiceRequest.class);
+    }
+}
