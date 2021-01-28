b835a54498b93e2fbd3c792be4e84b16c6700979
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-28 17:16:34 +0200

    added UserRepository

---
+++ src/main/java/com/company/backend/service/UserService.java
@@ -0,0 +1,15 @@
+package com.company.backend.service;
+
+import com.company.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}
