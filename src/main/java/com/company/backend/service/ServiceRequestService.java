6a763d2b1ceb6286ce97bad4fa41574ea8ada3e8
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-29 00:36:15 +0200

    implemented createServiceRequest method in ServiceRequestService

--- src/main/java/com/company/backend/service/ServiceRequestService.java
+++ src/main/java/com/company/backend/service/ServiceRequestService.java
@@ -1,5 +1,6 @@
 package com.company.backend.service;
 
+import com.company.backend.dto.NewServiceRequestDTO;
 import com.company.backend.dto.ServiceRequestDTO;
 import com.company.backend.entity.ServiceRequest;
 import com.company.backend.exeption.EntityNotFoundException;
@@ -15,20 +16,32 @@ public class ServiceRequestService {
 
     private ServiceRequestRepository serviceRequestRepository;
     private ServiceRequestMapper serviceRequestMapper;
+    private UserService userService;
 
-    public ServiceRequestService(ServiceRequestRepository serviceRequestRepository, ServiceRequestMapper serviceRequestMapper) {
+    public ServiceRequestService(ServiceRequestRepository serviceRequestRepository, ServiceRequestMapper serviceRequestMapper, UserService userService) {
         this.serviceRequestRepository = serviceRequestRepository;
         this.serviceRequestMapper = serviceRequestMapper;
+        this.userService = userService;
     }
 
     public List<ServiceRequestDTO> getAllServiceRequests() {
         return serviceRequestRepository.findAll().stream()
-                .map(serviceRequestMapper::convertServiceRequestToDTO)
+                .map(serviceRequestMapper::convertServiceRequestEntityToDTO)
                 .collect(Collectors.toList());
     }
 
     public ServiceRequestDTO getServiceRequestById(Long id) {
         ServiceRequest serviceRequest = serviceRequestRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(id));
-        return serviceRequestMapper.convertServiceRequestToDTO(serviceRequest);
+        return serviceRequestMapper.convertServiceRequestEntityToDTO(serviceRequest);
     }
+
+    public NewServiceRequestDTO createServiceRequest(NewServiceRequestDTO newServiceRequestDTO) {
+        ServiceRequest serviceRequest = serviceRequestMapper.convertNewServiceRequestDtoToEntity(newServiceRequestDTO);
+        // TODO: 1/28/21 Set logged in User in reportedBy field
+        serviceRequest.setReportedBy(userService.getUserEntityById(1L));
+        ServiceRequest savedServiceRequest = serviceRequestRepository.save(serviceRequest);
+        newServiceRequestDTO.setId(savedServiceRequest.getId());
+        return newServiceRequestDTO;
+    }
+
 }
