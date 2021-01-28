90de1d46e35eb9b4b781f4e624e247895ef5bcb9
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-28 15:49:19 +0200

    added OrganizationalUnitDTO

---
+++ src/main/java/com/company/backend/dto/OrganizationalUnitDTO.java
@@ -0,0 +1,18 @@
+package com.company.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class OrganizationalUnitDTO {
+
+    private Long id;
+
+    private String name;
+
+}
