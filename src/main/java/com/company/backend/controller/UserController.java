9c47eaadac73d3a8367aa9783cf4ec38d153a4ec
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-29 00:34:16 +0200

    added UserController

---
+++ src/main/java/com/company/backend/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.company.backend.controller;
+
+import com.company.backend.dto.UserDTO;
+import com.company.backend.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUser(@PathVariable Long id) {
+        return userService.getUserDtoById(id);
+    }
+
+}
