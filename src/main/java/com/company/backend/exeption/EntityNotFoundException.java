954b35e3af5b50ac7f27b3bfaad7c3641017ed7b
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-28 15:47:30 +0200

    added

---
+++ src/main/java/com/company/backend/exeption/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.company.backend.exeption;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(Long id) {
+        super("Entity with id: " + id + " was not found!");
+    }
+
+}
