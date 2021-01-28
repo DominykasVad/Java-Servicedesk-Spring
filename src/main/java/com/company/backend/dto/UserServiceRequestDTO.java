a641525a8f5716915956205f70a766a5e9df5488
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-01-29 00:33:52 +0200

    added UserServiceRequestDTO

---
+++ src/main/java/com/company/backend/dto/UserServiceRequestDTO.java
@@ -0,0 +1,29 @@
+package com.company.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserServiceRequestDTO {
+
+    private Long id;
+
+    private String summary;
+
+    private String ownerUsername;
+
+    private String companyName;
+
+    private String reportedByUsername;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
