c2bc4371a9400a6df053fedc655313e46272808d
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-01 02:33:06 +0200

    added Roles, RolesDTO

---
+++ src/main/java/com/company/backend/dto/RoleDTO.java
@@ -0,0 +1,18 @@
+package com.company.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class RoleDTO {
+
+    private Long id;
+
+    private String roleName;
+
+}
---
+++ src/main/java/com/company/backend/entity/Role.java
@@ -0,0 +1,27 @@
+package com.company.backend.entity;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + roleName;
+    }
+}
+
