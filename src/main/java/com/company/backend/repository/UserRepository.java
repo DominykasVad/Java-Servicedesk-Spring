c1ee2dde08feee111108162dc8f325c53b4608e7
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-01 02:30:08 +0200

    UserRepository: added methods to get User with roles

--- src/main/java/com/company/backend/repository/UserRepository.java
+++ src/main/java/com/company/backend/repository/UserRepository.java
@@ -2,8 +2,23 @@ package com.company.backend.repository;
 
 import com.company.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles")
+    List<User> findAllUsersWithRole();
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.id = :id")
+    Optional<User> findUserByIdWithRole(@Param("id") Long id);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.username = :username")
+    Optional<User> findUserByUsernameWithRole(@Param("username") String username);
+
 }
