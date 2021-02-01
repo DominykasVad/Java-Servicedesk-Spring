214df4f5b1becb9e5c224ecf105b2eead4e58e86
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-01 02:31:08 +0200

    UserService: modified methods to get User with roles

--- src/main/java/com/company/backend/service/UserService.java
+++ src/main/java/com/company/backend/service/UserService.java
@@ -5,13 +5,15 @@ import com.company.backend.entity.User;
 import com.company.backend.exeption.EntityNotFoundException;
 import com.company.backend.mapper.UserMapper;
 import com.company.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private UserRepository userRepository;
     private UserMapper userMapper;
@@ -22,17 +24,23 @@ public class UserService {
     }
 
     public User getUserEntityById(Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(id));
+        return userRepository.findUserByIdWithRole(id).orElseThrow(() -> new EntityNotFoundException(id));
     }
 
     public UserDTO getUserDtoById(Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException(id));
+        User user = userRepository.findUserByIdWithRole(id).orElseThrow(() -> new EntityNotFoundException(id));
         return userMapper.convertUserEntityToDTO(user);
     }
 
     public List<UserDTO> getAllUsers() {
-        return userRepository.findAll().stream()
+        return userRepository.findAllUsersWithRole().stream()
                 .map(userMapper::convertUserEntityToDTO)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public User loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUsernameWithRole(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User with username: %s Not found", username)));
     }
 }
