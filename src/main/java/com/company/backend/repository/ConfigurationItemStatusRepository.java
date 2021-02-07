71be9be8fb4393db0f564e2588bb01f91722536a
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-07 03:23:01 +0200

    Status repository refactor to ConfigurationItemStatus

---
+++ src/main/java/com/company/backend/repository/ConfigurationItemStatusRepository.java
@@ -0,0 +1,7 @@
+package com.company.backend.repository;
+
+import com.company.backend.entity.ConfigurationItemStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConfigurationItemStatusRepository extends JpaRepository<ConfigurationItemStatus, Long> {
+}
--- src/main/java/com/company/backend/repository/StatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.backend.repository;
-
-import com.company.backend.entity.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Long> {
-}
