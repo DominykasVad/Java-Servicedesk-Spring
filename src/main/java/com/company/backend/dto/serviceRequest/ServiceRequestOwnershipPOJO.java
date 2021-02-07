5cbb030b73457d288fe4b28751aaed70ee2cc720
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-07 19:50:13 +0200

    updated ServiceRequest DTOs

--- src/main/java/com/company/backend/dto/serviceRequest/ServiceRequestOwnershipPOJO.java
+++ src/main/java/com/company/backend/dto/serviceRequest/ServiceRequestOwnershipPOJO.java
@@ -5,26 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-public class NewServiceRequestDTO {
-
-    private Long id;
-
-    @NotEmpty
-    private String summary;
-
-    @NotEmpty
-    private String description;
+public class ServiceRequestOwnershipPOJO {
 
     @NotNull
-    private Long serviceRequestStatusId;
-
-    private Long organizationalUnitId;
+    private Long id;
 
 }
