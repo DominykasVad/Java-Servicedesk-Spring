8dbe84a4b76da7e62801e2b0e61ee3d0310a016b
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-07 03:09:08 +0200

    added ConfigurationItemController

---
+++ src/main/java/com/company/backend/controller/ConfigurationItemController.java
@@ -0,0 +1,70 @@
+package com.company.backend.controller;
+
+import com.company.backend.dto.ConfigurationItem.ConfigurationItemDTO;
+import com.company.backend.dto.ConfigurationItem.ConfigurationItemLinkPOJO;
+import com.company.backend.dto.ConfigurationItem.NewConfigurationItemDTO;
+import com.company.backend.dto.ConfigurationItem.UpdateConfigurationItemDTO;
+import com.company.backend.service.ConfigurationItemService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/ci")
+public class ConfigurationItemController {
+
+    private ConfigurationItemService configurationItemService;
+
+    public ConfigurationItemController(ConfigurationItemService configurationItemService) {
+        this.configurationItemService = configurationItemService;
+    }
+
+    @GetMapping
+    public Page<ConfigurationItemDTO> getConfigurationItems(@RequestParam(required = false, name = "serviceRequestId") Optional<Long> serviceRequestIdParam, Pageable pageable) {
+        if (serviceRequestIdParam.isPresent()) {
+            return configurationItemService.getConfigurationItemsByServiceRequestId(serviceRequestIdParam.get(), pageable);
+        }
+        return configurationItemService.getAllConfigurationItems(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ConfigurationItemDTO getConfigurationItem(@PathVariable Long id) {
+        return configurationItemService.getSingleConfigurationItem(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<ConfigurationItemDTO> addServiceRequest(@RequestBody @Valid NewConfigurationItemDTO newConfigurationItemDTO) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(configurationItemService.createConfigurationItem(newConfigurationItemDTO));
+    }
+
+    @PostMapping("/link-ci")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void linkConfigurationItemToServiceRequest(@RequestBody @Valid ConfigurationItemLinkPOJO configurationItemLinkPOJO) {
+        configurationItemService.linkConfigurationItemToServiceRequest(configurationItemLinkPOJO);
+    }
+
+    @PostMapping("/unlink-ci")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void unlinkConfigurationItemToServiceRequest(@RequestBody @Valid ConfigurationItemLinkPOJO configurationItemLinkPOJO) {
+        configurationItemService.unlinkConfigurationItemToServiceRequest(configurationItemLinkPOJO);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ConfigurationItemDTO updateConfigurationItem(@RequestBody @Valid UpdateConfigurationItemDTO updateConfigurationItemDTO) {
+        return configurationItemService.updateConfigurationItem(updateConfigurationItemDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteConfigurationItem(@PathVariable Long id) {
+        configurationItemService.deleteConfigurationItem(id);
+    }
+}
