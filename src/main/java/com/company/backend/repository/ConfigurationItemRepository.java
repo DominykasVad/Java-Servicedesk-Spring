9baba6079cda0af1f93d92d1f866370f2d627de7
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-07 03:23:21 +0200

    ConfigurationItemRepository updates

--- src/main/java/com/company/backend/repository/ConfigurationItemRepository.java
+++ src/main/java/com/company/backend/repository/ConfigurationItemRepository.java
@@ -1,7 +1,17 @@
 package com.company.backend.repository;
 
 import com.company.backend.entity.ConfigurationItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 public interface ConfigurationItemRepository extends JpaRepository<ConfigurationItem, Long> {
+
+    @Query(value = "SELECT ci FROM ConfigurationItem ci JOIN FETCH ci.serviceRequests sr WHERE sr.id = :serviceRequestId",
+            countQuery = "SELECT count(ci) FROM ConfigurationItem ci")
+    Page<ConfigurationItem> findAllConfigurationItemsByServiceRequestId(@Param("serviceRequestId") Long serviceRequestIde, Pageable pageable);
+
 }
