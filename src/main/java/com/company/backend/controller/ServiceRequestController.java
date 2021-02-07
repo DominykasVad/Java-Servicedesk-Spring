60b6e4881b6059c419f830ba8eff09ee2dcf80ba
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-07 03:10:41 +0200

    added Create new, edit, delete Service Request methods

--- src/main/java/com/company/backend/controller/ServiceRequestController.java
+++ src/main/java/com/company/backend/controller/ServiceRequestController.java
@@ -1,14 +1,17 @@
 package com.company.backend.controller;
 
-import com.company.backend.dto.NewServiceRequestDTO;
-import com.company.backend.dto.ServiceRequestDTO;
+import com.company.backend.dto.serviceRequest.UpdateServiceRequestDTO;
+import com.company.backend.dto.serviceRequest.NewServiceRequestDTO;
+import com.company.backend.dto.serviceRequest.ServiceRequestDTO;
 import com.company.backend.service.ServiceRequestService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/sr")
@@ -21,8 +24,11 @@ public class ServiceRequestController {
     }
 
     @GetMapping
-    public List<ServiceRequestDTO> getAllServiceRequests() {
-        return serviceRequestService.getAllServiceRequests();
+    public Page<ServiceRequestDTO> getAllServiceRequests(@RequestParam(required = false, name = "ownerId") Optional<Long> ownerId, Pageable pageable) {
+        if (ownerId.isPresent()) {
+            return serviceRequestService.getAllServiceRequestsByOwnerId(ownerId.get(), pageable);
+        }
+        return serviceRequestService.getAllServiceRequests(pageable);
     }
 
     @GetMapping("/{id}")
@@ -31,10 +37,22 @@ public class ServiceRequestController {
     }
 
     @PostMapping
-    public ResponseEntity<NewServiceRequestDTO> addServiceRequest(@RequestBody @Valid NewServiceRequestDTO newServiceRequestDTO) {
+    public ResponseEntity<ServiceRequestDTO> addServiceRequest(@RequestBody @Valid NewServiceRequestDTO newServiceRequestDTO) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(serviceRequestService.createServiceRequest(newServiceRequestDTO));
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ServiceRequestDTO updateServiceRequest(@RequestBody @Valid UpdateServiceRequestDTO updateServiceRequestDTO) {
+        return serviceRequestService.updateServiceRequest(updateServiceRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteServiceRequest(@PathVariable Long id) {
+        serviceRequestService.deleteServiceRequest(id);
     }
 
 }
