1a6a0179ca1c30d6c9dfaa0c5dbd271497a53459
Dominykas Vadeiša <domvadeisa@gmail.com>
2021-02-07 03:26:27 +0200

    updated ServiceRequestService

--- src/main/java/com/company/backend/service/ServiceRequestService.java
+++ src/main/java/com/company/backend/service/ServiceRequestService.java
@@ -1,16 +1,18 @@
 package com.company.backend.service;
 
-import com.company.backend.dto.NewServiceRequestDTO;
-import com.company.backend.dto.ServiceRequestDTO;
+import com.company.backend.dto.serviceRequest.NewServiceRequestDTO;
+import com.company.backend.dto.serviceRequest.ServiceRequestDTO;
+import com.company.backend.dto.serviceRequest.UpdateServiceRequestDTO;
 import com.company.backend.entity.ServiceRequest;
 import com.company.backend.exeption.EntityNotFoundException;
 import com.company.backend.mapper.ServiceRequestMapper;
 import com.company.backend.repository.ServiceRequestRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+@Slf4j
 @Service
 public class ServiceRequestService {
 
@@ -24,10 +26,14 @@ public class ServiceRequestService {
         this.userService = userService;
     }
 
-    public List<ServiceRequestDTO> getAllServiceRequests() {
-        return serviceRequestRepository.findAll().stream()
-                .map(serviceRequestMapper::convertServiceRequestEntityToDTO)
-                .collect(Collectors.toList());
+    public Page<ServiceRequestDTO> getAllServiceRequests(Pageable pageable) {
+        return serviceRequestRepository.findAll(pageable)
+                .map(serviceRequestMapper::convertServiceRequestEntityToDTO);
+    }
+
+    public Page<ServiceRequestDTO> getAllServiceRequestsByOwnerId(Long ownerId, Pageable pageable) {
+        return serviceRequestRepository.findAllByOwnerId(ownerId, pageable)
+                .map(serviceRequestMapper::convertServiceRequestEntityToDTO);
     }
 
     public ServiceRequestDTO getServiceRequestById(Long id) {
@@ -35,13 +41,22 @@ public class ServiceRequestService {
         return serviceRequestMapper.convertServiceRequestEntityToDTO(serviceRequest);
     }
 
-    public NewServiceRequestDTO createServiceRequest(NewServiceRequestDTO newServiceRequestDTO) {
+    public ServiceRequestDTO createServiceRequest(NewServiceRequestDTO newServiceRequestDTO) {
         ServiceRequest serviceRequest = serviceRequestMapper.convertNewServiceRequestDtoToEntity(newServiceRequestDTO);
         // TODO: 1/28/21 Set logged in User in reportedBy field
         serviceRequest.setReportedBy(userService.getUserEntityById(1L));
         ServiceRequest savedServiceRequest = serviceRequestRepository.save(serviceRequest);
-        newServiceRequestDTO.setId(savedServiceRequest.getId());
-        return newServiceRequestDTO;
+        return serviceRequestMapper.convertServiceRequestEntityToDTO(savedServiceRequest);
     }
 
+    public ServiceRequestDTO updateServiceRequest(UpdateServiceRequestDTO updateServiceRequestDTO) {
+        ServiceRequest updateServiceRequest = serviceRequestMapper.convertUpdateServiceRequestDtoToEntity(updateServiceRequestDTO);
+        ServiceRequest savedServiceRequest = serviceRequestRepository.save(updateServiceRequest);
+        return serviceRequestMapper.convertServiceRequestEntityToDTO(savedServiceRequest);
+    }
+
+    public void deleteServiceRequest(Long id) {
+        log.warn(String.format("User ID: %s Deleted Service Request ID: %s", 1, id));
+        serviceRequestRepository.deleteById(id);
+    }
 }
